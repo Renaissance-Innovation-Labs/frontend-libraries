846db68047eafb5a5e294eb4da49b9962640e263
droxy <chiomaotu22@gmail.com>
2024-06-19 17:08:10 +0100

    made component responsive

--- experiments/gallery-slider/.next/static/chunks/webpack.js
+++ experiments/gallery-slider/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fc5beb3faa8bbc5e"; }
+/******/ 		__webpack_require__.h = function() { return "6f1b0f7791b5d19f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- experiments/gallery-slider/components/GallerySlider.js/index.js
+++ experiments/gallery-slider/components/GallerySlider.js/index.js
@@ -21,7 +21,7 @@ const GallerySlider = ({ array }) => {
   };
 
   return (
-    <div className=" max-w-[1000px] mx-auto md:h-[410px] flex flex-col md:flex-row gap-2 mt-8 ">
+    <div className="max-w-[1000px] mx-auto md:h-[410px] flex gap-1 md:gap-2 mt-8 ">
       {array?.map((slide) => (
         <ImageSlide
           key={slide.id}
@@ -45,8 +45,8 @@ const ImageSlide = ({ slide, updateActive, activeImg }) => {
       onClick={() => updateActive(slide.id)}
       className={`${
         activeImg === slide?.id
-          ? 'h-80 w-full md:w-[400px] lg:w-[500px] grow-1 animate-slightFade'
-          : 'h-32 w-full md:w-16 lg:w-28 flex-none cursor-pointer'
+          ? 'h-80 w-4/5 lg:w-[500px] grow-1 animate-slightFade'
+          : 'h-80  w-4 md:w-16 lg:w-28 flex-none cursor-pointer'
       } md:h-full  relative rounded-2xl overflow-hidden bg-gray-200`}
     >
       <Image
@@ -59,7 +59,7 @@ const ImageSlide = ({ slide, updateActive, activeImg }) => {
 
       {slide?.text && activeImg === slide?.id ? (
         <div className="animate-fadeIn absolute bottom-[10%] text-white p-4">
-          <h4 className="font-bold text-2xl text-white">
+          <h4 className="font-bold text-xl lg:text-2xl text-white">
             {slide?.text?.header}
           </h4>
           <p className="text-xs text-white">{slide?.text?.sub}</p>
